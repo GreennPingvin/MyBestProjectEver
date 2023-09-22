75e0f11c1c73af2bff61a83b336bcbb6a874a38e
redo_oreon <sokolovskii.vlad2011@mail.ru>
2023-09-22 13:32:01 +0300

    add themes (normal, dark)

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import AboutPageAsync from "pages/AboutPage/AboutPage.async";
 import MainPageAsync from "pages/MainPage/MainPageAsync";
 import React, { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
-import "./index.scss";
+import "./styles/index.scss";
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle theme</button>
       <Link to={"/"}>Main</Link>
       <Link to={"/about"}>About</Link>
       <Suspense fallback={<h1>I am thinking...</h1>}>
